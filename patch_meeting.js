f47934dfa94b5e33e7b3ede3c045fcdb92c0c388
Stevens Dumpala <stevensdumpala@gmail.com>
2026-03-06 14:53:02 +0530

    Cleanup: Remove documentation and temporary patch scripts from repository

--- patch_meeting.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const file = '/var/www/goa.city/backend_node/src/controllers/meetings.controller.ts';
-let code = fs.readFileSync(file, 'utf8');
-code = code.replace('export const createMeeting = async (req: Request, res: Response) => {', "export const createMeeting = async (req: Request, res: Response) => {\n    console.log('CREATE MEETING CALLED:', req.body);");
-fs.writeFileSync(file, code);
