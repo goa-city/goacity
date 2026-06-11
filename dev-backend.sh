c1aad92831d0be80a1dd8de00fb091f21f810329
Stevens Dumpala <stevensdumpala@gmail.com>
2026-06-11 12:33:06 +0530

    Exclude dev scripts from repo

--- dev-backend.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd backend_node
-npm run dev
--- stop-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "🛑 Stopping all development servers..."
-
-# Kill processes on known ports
-lsof -ti:5173 | xargs kill -9 2>/dev/null
-lsof -ti:5001 | xargs kill -9 2>/dev/null
-
-echo "✅ Done."
